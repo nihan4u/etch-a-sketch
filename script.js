4a2f67e9ddb106e4fd342c242e107174b0c68572
nihan4u <nowa.uddin@gmail.com>
2024-05-03 10:11:46 -0400

    handle mouseleave

--- script.js
+++ script.js
@@ -1,19 +1,54 @@
 document.addEventListener('DOMContentLoaded', function() {
     const container = document.querySelector(".container");
+    let mouseDown = false;
+    const defaultSize = 16;
 
-for (i = 0; i < 16 * 16; i++) {
-    const div = document.createElement("div");
-    div.classList.add("grid-item");
-    container.appendChild(div);
+    createGrid(container, defaultSize); 
+    function createGrid(container, size) {
+        for (let i = 0; i < size * size; i++) {
+            const div = document.createElement("div");
+            div.classList.add("grid-item");
+            container.appendChild(div);
+    } 
+}
+const clearButton = document.querySelector('#clear-button');
+clearButton.addEventListener('click', clearGrid);
+
+container.addEventListener('mousedown', handleMouseDown);
+container.addEventListener('mouseover', handleMouseOver);
+container.addEventListener('mouseup', handleMouseUp);
+container.addEventListener('mouseleave', handleMouseLeave);
+
+function handleMouseDown(event) {
+    if (event.target.classList.contains('grid-item')) {
+        mouseDown = true;
+        changeColor(event.target);
+    }
 }
 
-const boxes = document.querySelectorAll('.grid-item')
-boxes.forEach (box => {
-    box.addEventListener('mouseover', function(event) {
-        const randomR = Math.floor(Math.random() * 256);
-        const randomG = Math.floor(Math.random() * 256);
-        const randomB = Math.floor(Math.random() * 256);
-        box.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`;
-    });
-});
+function handleMouseOver(event) {
+    if (mouseDown && event.target.classList.contains('grid-item')) {
+        changeColor(event.target);
+    }
+}
+
+function handleMouseUp() {
+    mouseDown = false;
+}
+
+function handleMouseLeave() {
+    mouseDown = false;
+}
+
+function changeColor(box) {
+    const randomR = Math.floor(Math.random() * 256);
+    const randomG = Math.floor(Math.random() * 256);
+    const randomB = Math.floor(Math.random() * 256);
+    box.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`;
+};
+
+function clearGrid() {
+    const gridItems = document.querySelectorAll('.grid-item');
+    gridItems.forEach(item => item.style.backgroundColor = '');
+}
 });
